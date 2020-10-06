e62f9e0d7a52d3564a7c9283f5a4e7a7545d4fe8
JunayedHassan <junayed09sojol@gmail.com>
2020-10-06 21:37:06 +0600

    server done

---
+++ data.js
@@ -0,0 +1,84 @@
+const eventData = [
+
+    {
+        "Name": "Child Support",
+        "pic": "https://ibb.co/qNvGF6S"
+    },
+    {
+        "Name": "Refuge Shelter",
+        "pic": "https://ibb.co/X7F1gpy"
+    },
+    {
+        "Name": "Food Charity",
+        "pic": "https://ibb.co/mGJjgzy"
+    },
+    {
+        "Name": "Host a clothing swap",
+        "pic": "https://ibb.co/H4bBnjK"
+    },
+    {
+        "Name": "Host a river clean - up",
+        "pic": "https://ibb.co/hgtnhZf"
+    },
+    {
+        "Name": "Clean water for children",
+        "pic": "https://ibb.co/vcWGn2s"
+    },
+    {
+        "Name": "Good education",
+        "pic": "https://ibb.co/TwQkCXH"
+    },
+    {
+        "Name": "New books for children",
+        "pic": "https://ibb.co/Jn5Jy90"
+    },
+    {
+        "Name": "Host a study group",
+        "pic": "https://ibb.co/Xy10gc1"
+    },
+    {
+        "Name": "Build birdhouses for your neighbors",
+        "pic": "https://ibb.co/VHdrrH5"
+    },
+    {
+        "Name": "Organize books at the library",
+        "pic": "https://ibb.co/Kbw8Hvp"
+    },
+    {
+        "Name": "Give a seminar on driving safety",
+        "pic": "https://ibb.co/x8DKqpd"
+    },
+    {
+        "Name": "Give free music lessons",
+        "pic": "https://ibb.co/nMYXbgB"
+    },
+    {
+        "Name": "Teach people how to register to vote",
+        "pic": "https://ibb.co/BcFqggL"
+    },
+    {
+        "Name": "Clean up your local park",
+        "pic": "https://ibb.co/P56Ckgh"
+    },
+    {
+        "Name": "Give IT help to local adults",
+        "pic": "https://ibb.co/bK2nzqS"
+    },
+    {
+        "Name": "Foster a shelter animal",
+        "pic": "https://ibb.co/QdrvS9n"
+    },
+    {
+        "Name": "Babysit during PTA meetings",
+        "pic": "https://ibb.co/9qjLcD6"
+    },
+    {
+        "Name": "Collect stuffed animals",
+        "pic": "https://ibb.co/z4DJGdD"
+    },
+    {
+        "Name": "Collect school supplies",
+        "pic": "https://ibb.co/GxyzgsH",
+    }
+];
+export default eventData;
--- index.js
+++ index.js
@@ -1,9 +1,81 @@
+const bodyParser = require('body-parser');
 const express = require('express');
-
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config();
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ejye6.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.send("Thanks for calling me")
+app.use(bodyParser.json());
+app.use(cors());
+
+
+const port = 3100;
+app.get('/', (req, res) => {
+    res.send("Thanks calling me")
 })
 
-app.listen(3000, () => console.log('object'));
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const eventsCollection = client.db(`${process.env.DB_NAME}`).collection(`${process.env.DB_COLLECTION_NAME}`);
+    app.post('/addevents', (req, res) => {
+        const event = req.body;
+        eventsCollection.insertOne(event)
+            .then(result => {
+                console.log(result.insertedCount)
+                res.sendStatus(result.insertedCount)
+            })
+    })
+
+
+    const registersCollection = client.db(`${process.env.DB_NAME}`).collection(`${process.env.DB_COLLECTION_REGISTER}`);
+    app.post('/addregisters', (req, res) => {
+        const register = req.body;
+        registersCollection.insertOne(register)
+            .then(result => {
+                console.log(result.insertedCount)
+                res.sendStatus(result.insertedCount)
+            })
+    })
+    app.get('/event-registers', (req, res) => {
+        registersCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+
+    app.get('/events', (req, res) => {
+        eventsCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    app.delete('/delete/:id', (req, res) => {
+        registersCollection.deleteOne({_id: ObjectId(req.params.id)})
+        .then(  result => {
+            console.log(result);
+        })
+    })
+    
+    app.delete('/deleteRegistry/:id', (req, res) => {
+        registersCollection.deleteOne({_id: ObjectId(req.params.id)})
+        .then(  result => {
+            console.log(result);
+        })
+    })
+
+
+
+
+});
+
+
+
+
+
+
+app.listen(port);
